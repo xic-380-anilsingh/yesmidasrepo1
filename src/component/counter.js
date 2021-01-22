b0389d2613c645f3b248a89b9344422bcac26695
Anil Singh <anil.singh@xebia.com>
2021-01-22 16:07:50 +0530

    Added dropdown

--- src/component/counter.js
+++ src/component/counter.js
@@ -30,7 +30,7 @@ import {
       
       }  
     render() {
-      const DEMO_OPTIONS = ['option 1', 'option 2', 'option 3', 'option 4', 'option 5', 'option 6', 'option 7', 'option 8', 'option 9'];
+      const DEMO_OPTIONS = ['option 1', 'option 2', 'option 3', 'option 4', 'option 5', 'option 6', 'option 7', 'option 8'];
         return (
             <View style={styles.container}>
              
